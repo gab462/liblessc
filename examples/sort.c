b9574b113a919be99e44e377014d5b670a367b5f
gab462 <gustavoaraujob1+1@gmail.com>
2023-08-20 10:57:59 -0300

    Implement sorting visualization

--- examples/sort.c
+++ examples/sort.c
@@ -1,13 +1,21 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h> //usleep
 
 #include "tui.h"
 
 #define WIDTH 80
 #define HEIGHT 24
 
-void render(struct tui *tui, int arr[WIDTH]);
+void init_array(int arr[WIDTH]);
+
+void bubble(struct tui *tui, int arr[WIDTH]);
+void selection(struct tui *tui, int arr[WIDTH]);
+void insertion(struct tui *tui, int arr[WIDTH]);
+void merge(struct tui *tui, int arr[WIDTH]);
+void quick(struct tui *tui, int arr[WIDTH]);
 
 int main(void)
 {
@@ -19,17 +27,17 @@ int main(void)
 		.height = HEIGHT
 	};
 
-	srand(time(NULL));
-
 	int arr[WIDTH];
 
-	for (int i = 0; i < WIDTH; ++i) {
-		arr[i] = rand() % HEIGHT;
-	}
+	srand(time(NULL));
 
 	setup_term();
 
-	render(&tui, arr);
+	init_array(arr);
+
+	//bubble(&tui, arr);
+	//selection(&tui, arr);
+	insertion(&tui, arr);
 
 	getchar();
 
@@ -39,16 +47,91 @@ int main(void)
 	return 0;
 }
 
-void render(struct tui *tui, int arr[WIDTH])
+void init_array(int arr[WIDTH])
+{
+	for (int i = 0; i < WIDTH; ++i) {
+		arr[i] = rand() % HEIGHT;
+	}
+}
+
+void render(struct tui *tui, int arr[WIDTH], int ptr)
 {
 	fill_tui(tui, ' ');
 
 	for (int y = 0; y < tui->height; ++y) {
 		for (int x = 0; x < tui->width; ++x) {
-			if (arr[x] >= HEIGHT - y)
-				set_char(tui, x, y, '#');
+			char c = x == ptr ? '*' : '#';
+
+			if (arr[x] >= tui->height - y)
+				set_char(tui, x, y, c);
 		}
 	}
 
 	refresh_tui(tui);
+
+	usleep(50000);
+}
+
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void bubble(struct tui *tui, int arr[WIDTH])
+{
+	bool swapped;
+
+	do {
+		swapped = false;
+
+		for (int i = 1; i < WIDTH; ++i) {
+			render(tui, arr, i);
+
+			if (arr[i - 1] > arr[i]) {
+				swap(&arr[i - 1], &arr[i]);
+
+				swapped = true;
+
+				render(tui, arr, i);
+			}
+		}
+	} while (swapped);
+}
+
+void selection(struct tui *tui, int arr[WIDTH])
+{
+	for (int i = 0; i < WIDTH - 1; ++i) {
+		int min = i;
+
+		for (int j = i + 1; j < WIDTH; ++j) {
+			if (arr[j] < arr[min])
+				min = j;
+
+			render(tui, arr, j);
+		}
+
+		if (min != i)
+			swap(&arr[i], &arr[min]);
+	}
+}
+
+void insertion(struct tui *tui, int arr[WIDTH])
+{
+	for (int i = 1; i < WIDTH; ++i) {
+		for (int j = i; j > 0 && arr[j - 1] > arr[j]; --j) {
+			swap(&arr[j], &arr[j - 1]);
+
+			render(tui, arr, j - 1);
+		}
+	}
+}
+
+void merge(struct tui *tui, int arr[WIDTH])
+{
+}
+
+void quick(struct tui *tui, int arr[WIDTH])
+{
 }
